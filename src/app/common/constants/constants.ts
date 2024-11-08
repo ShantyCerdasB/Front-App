3306ac87b0c723130c803e359438eeeeb178585d
Shanty <163443786+ShantyCerdasB@users.noreply.github.com>
2024-11-08 12:35:50 -0600

    Update constants.ts

--- src/app/common/constants/constants.ts
+++ src/app/common/constants/constants.ts
@@ -1,7 +1,7 @@
 import { UnmutableConstants } from "./unmutable-constans";
 
 export class Constants extends UnmutableConstants {
-  static readonly appHost = "http://localhost:3000/";
+  static readonly appHost = "http://ec2-54-193-205-101.us-west-1.compute.amazonaws.com:3000";
   //static readonly appHost = "https://app-cafe-don-emilio.com/";
   static readonly apiBaseUrl = "CafeDonEmilioWS/api";
 
