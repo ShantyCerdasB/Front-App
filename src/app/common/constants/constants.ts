f56d186fbca6b6ca786baffd5985a65a6589dccf
Shanty <163443786+ShantyCerdasB@users.noreply.github.com>
2024-11-16 23:08:45 -0600

    deploy automatico prueba

--- src/app/common/constants/constants.ts
+++ src/app/common/constants/constants.ts
@@ -1,9 +1,12 @@
 import { UnmutableConstants } from "./unmutable-constans";
 
 export class Constants extends UnmutableConstants {
-  static readonly appHost = "http://localhost:3000/";
+  static readonly appHost =
+    "https://backend-web-app-cafe-don-emilio.onrender.com/";
   //static readonly appHost = "https://app-cafe-don-emilio.com/";
   static readonly apiBaseUrl = "CafeDonEmilioWS/api";
+
+  static readonly fullUrlParcial = `${Constants.appHost}${Constants.apiBaseUrl}`;
 
   static readonly modoOffline = false;
 }
--- src/app/common/constants/unmutable-constans.ts
+++ src/app/common/constants/unmutable-constans.ts
@@ -4,5 +4,12 @@ export class UnmutableConstants {
   static readonly language = "language";
 
   private static readonly products = "/products";
-  static readonly getAllProducts = UnmutableConstants.products + "/all";
+  static readonly getAllProducts = UnmutableConstants.products;
+
+  private static readonly order = "/orden-compra";
+  static readonly orderPurchase  = UnmutableConstants.order;
+
+  private static readonly reservation = "/reserva";
+  static readonly reservationOrder  = UnmutableConstants.reservation;
+
 }
